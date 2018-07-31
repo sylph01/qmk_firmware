4b9cd7c7d4ccfc43a0a9d12aed4b5ad0be77e795
Ryo Kajiwara <kfe-fecn6.prussian@s01.info>
2018-08-01 06:36:45 +0900

    minor change in keymaps + tapping action

--- keyboards/iris/keymaps/sylph01/keymap.c
+++ keyboards/iris/keymaps/sylph01/keymap.c
@@ -18,6 +18,7 @@ enum custom_keycodes {
 
 #define KC_ KC_TRNS
 #define _______ KC_TRNS
+#define XXXXXXX KC_NO
 
 #define KC_LOWR LOWER
 #define KC_RASE RAISE
@@ -35,46 +36,64 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  [_QWERTY] = LAYOUT_kc(
+  [_QWERTY] =
   //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
-     ESC , 1  , 2  , 3  , 4  , 5  ,                6  , 7  , 8  , 9  , 0  ,BSPC,
+  //|ESC , 1  , 2  , 3  , 4  , 5  ,                6  , 7  , 8  , 9  , 0  ,BSPC,
   //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-     TAB , Q  , W  , E  , R  , T  ,                Y  , U  , I  , O  , P  ,MINS,
+  //|TAB , Q  , W  , E  , R  , T  ,                Y  , U  , I  , O  , P  ,MINS,
   //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-     LCTL, A  , S  , D  , F  , G  ,                H  , J  , K  , L  ,SCLN,QUOT,
+  //|LCTL, A  , S  , D  , F  , G  ,                H  , J  , K  , L  ,SCLN,QUOT,
   //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
-     LSFT, Z  , X  , C  , V  , B  ,    ,         , N  , M  ,COMM,DOT ,SLSH,RSFT,
+  //|LSFT, Z  , X  , C  , V  , B  ,    ,         , N  , M  ,COMM,DOT ,SLSH,RSFT,
   //`----+----+----+--+-+----+----+----/    \----+----+----+----+----+----+----'
-                       LGUI,LOWR,SPC ,         ENT ,RASE,RALT
+  //                   LGUI,LOWR,SPC ,         ENT ,RASE,RALT
   //                  `----+----+----'        `----+----+----'
+  LAYOUT(
+    KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                   KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_BSPC,
+    KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                   KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_MINS,
+    KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                   KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
+    KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , _______, _______, KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
+    KC_LGUI, LT(KC_LOWR, KC_MHEN), KC_SPC , KC_ENT , LT(KC_RASE, KC_HENK), KC_RALT
   ),
 
-  [_LOWER] = LAYOUT_kc(
+  [_LOWER] =
   //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
-     TILD,EXLM, AT ,HASH,DLR ,PERC,               CIRC,AMPR,ASTR,LPRN,RPRN,DEL ,
+  //|TILD,EXLM, AT ,HASH,DLR ,PERC,               CIRC,AMPR,ASTR,LPRN,RPRN,DEL ,
   //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-     KANA, 1  , 2  , 3  , 4  , 5  ,                6  , 7  , 8  , 9  , 0  ,    ,
+  //|LALT, 1  , 2  , 3  , 4  , 5  ,                6  , 7  , 8  , 9  , 0  ,    ,
   //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-     LCTL,    ,    ,    ,    ,LBRC,               RBRC,    , UP ,    ,PLUS,ENT ,
+  //|LCTL,LALT,    ,    ,    ,LBRC,               RBRC,    , UP ,    ,PLUS,ENT ,
   //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
-     BL_S,    ,    ,    ,    ,LCBR,    ,         ,RCBR,LEFT,DOWN,RGHT,MINS,PIPE,
+  //|LSFT,    ,    ,    ,    ,LCBR,    ,         ,RCBR,LEFT,DOWN,RGHT,MINS,PIPE,
   //`----+----+----+--+-+----+----+----/    \----+----+----+----+----+----+----'
-                       MHEN,    ,ENT ,         SPC ,    ,HENK
+  //                   MHEN,    ,ENT ,         SPC ,    ,HENK
   //                  `----+----+----'        `----+----+----'
+  LAYOUT(
+    KC_TILD, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
+    KC_LALT, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                   KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , _______,
+    KC_LCTL, _______, _______, _______, _______, KC_LBRC,                   KC_RBRC, _______, KC_UP  , _______, KC_PLUS, KC_QUOT,
+    KC_LSFT, _______, _______, _______, _______, KC_LCBR, _______, _______, KC_RCBR, KC_LEFT, KC_DOWN, KC_RGHT, KC_MINS, KC_RSFT,
+    KC_MHEN, _______, KC_ENT , KC_SPC , _______, KC_HENK
   ),
 
-  [_RAISE] = LAYOUT_kc(
+  [_RAISE] =
   //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
-     F12 , F1 , F2 , F3 , F4 , F5 ,                F6 , F7 , F8 , F9 ,F10 ,F11 ,
+  //|F12 , F1 , F2 , F3 , F4 , F5 ,                F6 , F7 , F8 , F9 ,F10 ,F11 ,
   //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-         ,EXLM, AT ,HASH,DLR ,PERC,               CIRC,AMPR,ASTR,LPRN,RPRN,    ,
+  //|    ,EXLM, AT ,HASH,DLR ,PERC,               CIRC,AMPR,ASTR,LPRN,RPRN,    ,
   //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-         ,MPRV,MNXT,VOLU,PGUP,UNDS,               EQL ,HOME,    ,    ,    ,BSLS,
+  //|    ,    ,    ,    ,PGUP,UNDS,               EQL ,HOME,    ,    ,    ,    ,
   //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
-     MUTE,MSTP,MPLY,VOLD,PGDN,MINS,    ,         ,PLUS,END ,    ,    ,BSLS,    ,
+  //|    ,    ,    ,    ,PGDN,MINS,    ,         ,PLUS,END ,    ,    ,BSLS,    ,
   //`----+----+----+--+-+----+----+----/    \----+----+----+----+----+----+----'
-                       MHEN,    ,ENT ,         SPC ,    ,HENK
+  //                   MHEN,    ,ENT ,         SPC ,    ,HENK
   //                  `----+----+----'        `----+----+----'
+  LAYOUT(
+    KC_F12 , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,                   KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 ,
+    _______, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
+    _______, _______, _______, _______, _______, KC_UNDS,                   KC_EQL , KC_HOME, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, KC_MINS, _______, _______, KC_PLUS, KC_END , _______, _______, KC_BSLS, _______,
+    KC_MHEN, _______, KC_ENT , KC_SPC , _______, KC_HENK
   ),
 
   [_ADJUST] = LAYOUT_kc(
