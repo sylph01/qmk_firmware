8a15c42d9763f47168eab4de6d1f709905ae0176
Ryo Kajiwara <kfe-fecn6.prussian@s01.info>
2018-08-01 06:48:20 +0900

    Fix layer switch

--- keyboards/iris/keymaps/sylph01/keymap.c
+++ keyboards/iris/keymaps/sylph01/keymap.c
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                   KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_MINS,
     KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                   KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , _______, _______, KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
-    KC_LGUI, LT(KC_LOWR, KC_MHEN), KC_SPC , KC_ENT , LT(KC_RASE, KC_HENK), KC_RALT
+    KC_LGUI, LT(M(_LOWER), KC_MHEN), KC_SPC , KC_ENT , LT(M(_RAISE), KC_HENK), KC_RALT
   ),
 
   [_LOWER] =
