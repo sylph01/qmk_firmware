684208ad11e0f8700cc680a4c8a84c45e9671957
Ryo Kajiwara <kfe-fecn6.prussian@s01.info>
2018-12-14 19:47:34 +0900

    Initial version of Fortitude60 sylph01 keymap

--- keyboards/fortitude60/keymaps/sylph01/config.h
+++ keyboards/fortitude60/keymaps/sylph01/config.h
@@ -36,7 +36,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* #define RGBLIGHT_SAT_STEP 8 */
 /* #define RGBLIGHT_VAL_STEP 8 */
 
-#define PERMISSIVE_HOLD
-#define PREVENT_STUCK_MODIFIERS
-
 #endif
