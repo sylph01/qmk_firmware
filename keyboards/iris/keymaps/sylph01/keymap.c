523733e61dfbcf820e92bade15620501154bbef3
Ryo Kajiwara <sylph01@s01.ninja>
2019-03-29 14:55:31 +0900

    [iris, f60] Reviewed keymap

--- keyboards/iris/keymaps/sylph01/keymap.c
+++ keyboards/iris/keymaps/sylph01/keymap.c
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                   KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_BSPC,
     KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                   KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_MINS,
     KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                   KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
-    KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , _______, _______, KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
+    KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_LBRC, KC_RBRC, KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
     KC_LGUI, LT(M(_LOWER), KC_MHEN), KC_SPC , KC_ENT , LT(M(_RAISE), KC_HENK), KC_RALT
   ),
 
@@ -69,11 +69,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //                   MHEN,    ,    ,             ,    ,HENK
   //                  `----+----+----'        `----+----+----'
   LAYOUT(
-    KC_TILD, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL ,
+    KC_ESC , KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL ,
     KC_LALT, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                   KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_GRV ,
-    KC_LCTL, _______, _______, _______, _______, KC_LBRC,                   KC_RBRC, _______, KC_UP  , KC_MINS, KC_PLUS, KC_PIPE,
-    KC_LSFT, _______, _______, _______, _______, KC_LCBR, _______, _______, KC_RCBR, KC_LEFT, KC_DOWN, KC_RGHT, KC_BSLS, KC_RSFT,
-    KC_MHEN, _______, _______, _______, _______, KC_HENK
+    _______, _______, _______, KC_QUOT, KC_HOME, KC_LBRC,                   KC_RBRC, KC_END , KC_UP  , KC_MINS, KC_PLUS, KC_PIPE,
+    _______, _______, _______, KC_PIPE, KC_RABK, KC_LCBR, KC_UNDS, KC_UNDS, KC_RCBR, KC_LEFT, KC_DOWN, KC_RGHT, KC_BSLS, _______,
+    _______, _______, _______, _______, _______, _______
   ),
 
   [_RAISE] =
@@ -90,10 +90,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //                  `----+----+----'        `----+----+----'
   LAYOUT(
     KC_F12 , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,                   KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 ,
-    _______, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
-    _______, _______, _______, KC_PSCR, KC_PGUP, KC_UNDS,                   KC_EQL , KC_HOME, _______, _______, _______, _______,
-    _______, _______, _______, _______, KC_PGDN, KC_MINS, _______, _______, KC_PLUS, KC_END , _______, _______, KC_BSLS, _______,
-    KC_MHEN, _______, _______, _______, _______, KC_HENK
+    _______, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_GRV ,
+    _______, _______, _______, _______, _______, KC_LPRN,                   KC_RPRN, _______, _______, _______, _______, KC_QUOT,
+    _______, KC_PSCR, _______, KC_PGUP, KC_MINS, KC_UNDS, _______, _______, KC_EQL , KC_PLUS, KC_PGDN, _______, KC_BSLS, _______,
+    _______, _______, _______, _______, _______, _______
   ),
 
   [_ADJUST] = LAYOUT_kc(
